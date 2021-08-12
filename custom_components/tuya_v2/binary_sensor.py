f951e6c84ef41a135032135929906207bda48272
petebdeblois <petebdeblois@gmail.com>
2021-08-12 02:51:10 -0400

    config files on 12-08-2021 02:51:10

--- custom_components/tuya_v2/binary_sensor.py
+++ custom_components/tuya_v2/binary_sensor.py
@@ -2,12 +2,15 @@
 """Support for Tuya Binary Sensor."""
 
 import logging
+import json
 from typing import Callable, List, Optional
 
 from tuya_iot import TuyaDevice, TuyaDeviceManager
+from threading import Timer
 
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_DOOR,
+    DEVICE_CLASS_GARAGE_DOOR,
     DEVICE_CLASS_GAS,
     DEVICE_CLASS_MOISTURE,
     DEVICE_CLASS_MOTION,
@@ -41,7 +44,8 @@ TUYA_SUPPORT_TYPE = [
     "sj",  # Water Detector
     "sos",  # Emergency Button
     "hps",  # Human Presence Sensor
-    "ms", # Residential Lock
+    "ms",  # Residential Lock
+    "ckmkzq",  # Garage Door Opener
 ]
 
 # Door Window Sensor
@@ -70,7 +74,8 @@ async def async_setup_entry(
     """Set up tuya binary sensors dynamically through tuya discovery."""
     _LOGGER.info("binary sensor init")
 
-    hass.data[DOMAIN][TUYA_HA_TUYA_MAP].update({DEVICE_DOMAIN: TUYA_SUPPORT_TYPE})
+    hass.data[DOMAIN][TUYA_HA_TUYA_MAP].update(
+        {DEVICE_DOMAIN: TUYA_SUPPORT_TYPE})
 
     async def async_discover_device(dev_ids):
         """Discover and add a discovered tuya sensor."""
@@ -108,15 +113,21 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_DOOR,
+                    DPCODE_DOORLOCK_STATE,
                     (lambda d: d.status.get(DPCODE_DOORLOCK_STATE, "none") != "closed"),
                 )
             )
         if DPCODE_DOORCONTACT_STATE in device.status:
+            if device.category == "ckmkzq":
+                device_class_d = DEVICE_CLASS_GARAGE_DOOR
+            else:
+                device_class_d = DEVICE_CLASS_DOOR
             entities.append(
                 TuyaHaBSensor(
                     device,
                     device_manager,
-                    DEVICE_CLASS_DOOR,
+                    device_class_d,
+                    DPCODE_DOORCONTACT_STATE,
                     (lambda d: d.status.get(DPCODE_DOORCONTACT_STATE, False)),
                 )
             )
@@ -126,6 +137,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_DOOR,
+                    DPCODE_SWITCH,
                     (lambda d: d.status.get(DPCODE_SWITCH, False)),
                 )
             )
@@ -135,6 +147,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_SMOKE,
+                    DPCODE_SMOKE_SENSOR_STATE,
                     (lambda d: d.status.get(DPCODE_SMOKE_SENSOR_STATE, 1) == "1"),
                 )
             )
@@ -144,7 +157,9 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_SMOKE,
-                    (lambda d: d.status.get(DPCODE_SMOKE_SENSOR_STATUS, 'normal') == "alarm"),
+                    DPCODE_SMOKE_SENSOR_STATUS,
+                    (lambda d: d.status.get(
+                        DPCODE_SMOKE_SENSOR_STATUS, 'normal') == "alarm"),
                 )
             )
         if DPCODE_BATTERY_STATE in device.status:
@@ -153,6 +168,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_BATTERY,
+                    DPCODE_BATTERY_STATE,
                     (lambda d: d.status.get(DPCODE_BATTERY_STATE, 'normal') == "low"),
                 )
             )
@@ -162,6 +178,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOTION,
+                    DPCODE_TEMPER_ALRAM,
                     (lambda d: d.status.get(DPCODE_TEMPER_ALRAM, False)),
                 )
             )
@@ -171,6 +188,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_GAS,
+                    DPCODE_GAS_SENSOR_STATE,
                     (lambda d: d.status.get(DPCODE_GAS_SENSOR_STATE, 1) == "1"),
                 )
             )
@@ -180,6 +198,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOTION,
+                    DPCODE_PIR,
                     (lambda d: d.status.get(DPCODE_PIR, "none") == "pir"),
                 )
             )
@@ -189,7 +208,9 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOISTURE,
-                    (lambda d: d.status.get(DPCODE_WATER_SENSOR_STATE, "normal") == "alarm"),
+                    DPCODE_WATER_SENSOR_STATE,
+                    (lambda d: d.status.get(
+                        DPCODE_WATER_SENSOR_STATE, "normal") == "alarm"),
                 )
             )
         if DPCODE_SOS_STATE in device.status:
@@ -198,6 +219,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_PROBLEM,
+                    DPCODE_SOS_STATE,
                     (lambda d: d.status.get(DPCODE_SOS_STATE, False)),
                 )
             )
@@ -207,6 +229,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOTION,
+                    DPCODE_PRESENCE_STATE,
                     (
                         lambda d: d.status.get(DPCODE_PRESENCE_STATE, "none")
                         == "presence"
@@ -225,22 +248,24 @@ class TuyaHaBSensor(TuyaHaDevice, BinarySensorEntity):
         device: TuyaDevice,
         device_manager: TuyaDeviceManager,
         sensor_type: str,
+        sensor_code: str,
         sensor_is_on: Callable[..., bool],
     ):
         """Init TuyaHaBSensor."""
         self._type = sensor_type
+        self._code = sensor_code
         self._is_on = sensor_is_on
         super().__init__(device, device_manager)
 
     @property
     def unique_id(self) -> Optional[str]:
         """Return a unique ID."""
-        return f"{super().unique_id}{self._type}"
+        return f"{super().unique_id}{self._code}"
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self.tuya_device.name + "_" + self._type
+        return self.tuya_device.name + "_" + self._code
 
     @property
     def is_on(self):
@@ -256,3 +281,19 @@ class TuyaHaBSensor(TuyaHaDevice, BinarySensorEntity):
     def available(self) -> bool:
         """Return if the device is available."""
         return True
+
+    def reset_pir(self):
+        self.tuya_device.status[DPCODE_PIR] = "none"
+        self.schedule_update_ha_state()
+
+    def schedule_update_ha_state(self, force_refresh: bool = False) -> None:
+
+        if self._code == DPCODE_PIR:
+            pir_range = json.loads(self.tuya_device.status_range.get(DPCODE_PIR, {}).values).get(
+                "range"
+            )
+            if len(pir_range) == 1 and self.tuya_device.status[DPCODE_PIR] == "pir":
+                timer = Timer(10, lambda: self.reset_pir())
+                timer.start()
+
+        super().schedule_update_ha_state(force_refresh)
