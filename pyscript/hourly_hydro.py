349258f8643624699673036a6f41bff97ad60060
petebdeblois <petebdeblois@gmail.com>
2021-09-03 16:25:59 -0400

    update gitignore

--- pyscript/hourly_hydro.py
+++ pyscript/hourly_hydro.py
@@ -30,30 +30,35 @@ list_var = ['yesterday_hourly_consumption_00',
 ]
 list_consumption = {}
 list_hour = {}
-list_date = None
-hydro_yesterday_cost = None
+hydro_yesterday_cost = 0
 hydro_access_fee = 0.4116
 lower_price = 0.0619
 higher_price = 0.095
+
 
 @pyscript_compile
 def hydro_open():
   with open('config/data/data.json') as f:
     data = json.load(f)
+  time.sleep(5)
+  global yesterday_higher
+  global yesterday_lower
+  global list_date
+  global hydro_yesterday_cost
   yesterday_lower = data['yesterday_data']['lower_price_consumption']
   yesterday_higher = data['yesterday_data']['higher_price_consumption']
-  log.info("yesterday_lower " + yesterday_lower)
-  hydro_yesterday_cost = hydro_access_fee + (float(yesterday_lower) * lower_price) + (float(yesterday_higher) * higher_price)
-  #log.info("FIrst " + hydro_yesterday_cost)
+  hydro_yesterday_cost = round(hydro_access_fee + (float(yesterday_lower) * lower_price) + (float(yesterday_higher) * higher_price),2)
   list_date = data['yesterday_data']['date']
   for i in range(len(data['hourly_data'])):
     list_consumption[i] = data['hourly_data'][i]['total_consumption']
     list_hour[i] = data['hourly_data'][i]['hour']
 
-@time_trigger("once(16:01:00)")
+@time_trigger("once(06:30:30)")
 def hydro_states():
     hydro_open()
-    time.sleep(5)
+    state.set("sensor.hydro_yesterday_cost", value=hydro_yesterday_cost)
+    state.setattr("sensor.hydro_yesterday_cost.unit_of_measurement", "$")
+    state.setattr("sensor.hydro_yesterday_cost.friendly_name", "Cost of yesterdays electrical Consumption")
     for i in range(len(list_var)):
       state.set("sensor." + list_var[i], value=list_consumption[i],)
       state.setattr("sensor." + list_var[i]+ ".unit_of_measurement", "Kwh")
@@ -64,7 +69,4 @@ def hydro_states():
         state.setattr("sensor." + list_var[i]+ ".friendly_name", "Consumption at " + str(list_hour[i]) )
       state.setattr("sensor." + list_var[i]+ ".icon", "mdi:flash" )
       state.setattr("sensor." + list_var[i]+ ".date", "Consumption on " + str(list_date) )
-    state.set("sensor.hydro_yesterday_cost", value=hydro_yesterday_cost)
-    log.info("Final " + hydro_yesterday_cost)
-    log.info("Hourly hydro script has run")
 
--- pyscript/school_summer_vacation.py
+++ pyscript/school_summer_vacation.py
@@ -1,11 +1,17 @@
+'''
+Created a script to keep track summer vacation. This will be usueful for automation based on school days
+'''
 from datetime import date
-
-@time_trigger("once(21:46:00)")
+state.persist('pyscript.school_summer_vacation')
+@time_trigger("once(08:06:50)")
 def summer_vacation():
+    log.debug("yeah")
     today = date.today()
     d = int(today.strftime("%d"))
     m = int(today.strftime("%m"))
     if d == 28 and m == 8:
-        log.info('yes ' + str(d) + str(m))
-    else:
-        log.info('no ' + str(d) + str(m))
+      log.info('yes')
+      state.set("input_boolean.school_summer_vacation", value="off")
+    elif d == 23 and m == 6:
+      log.info('no')
+      state.set("input_boolean.school_summer_vacation", value="on")
