7477fca0dd877b3711150702d56c1c4749cda779
petebdeblois <petebdeblois@gmail.com>
2021-11-05 02:51:03 -0400

    config files on 05-11-2021 02:51:03

--- pyscript/test.py
+++ pyscript/test.py
@@ -3,7 +3,7 @@ Service script
 """
 @service
 def test():
-    if binary_sensor.workday == 'on':
+    if binary_sensor.workday_sensor == 'on':
         log.info(f"Service Trigger test: fail")
         pass
     else:
