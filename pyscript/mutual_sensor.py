b37298dc027fef9a88ebb0c803c6c8b1968148d9
petebdeblois <petebdeblois@gmail.com>
2021-11-14 02:51:04 -0500

    config files on 14-11-2021 02:51:04

--- pyscript/mutual_sensor.py
+++ pyscript/mutual_sensor.py
@@ -19,23 +19,28 @@ def read():
   time.sleep(5)
 
 
-@time_trigger("once(06:19:45)")
+@time_trigger("once(18:49:20)")
 def write():
   read()
 
-  state.set("sensor.mmf_holding_celi", value=data['holding'][0]['holding_celi'])
-  state.setattr("sensor.mmf_holding_celi" + ".unit_of_measurement", "$")
+  state.set("sensor.finance_mmf_holding_celi", value=data['holding_mmf'][0]['holding_celi'])
+  state.setattr("sensor.finance_mmf_holding_celi" + ".unit_of_measurement", "$")
   
-  state.set("sensor.mmf_holding_reer", value=data['holding'][0]['holding_reer'])
-  state.setattr("sensor.mmf_holding_reer" + ".unit_of_measurement", "$")
+  state.set("sensor.finance_mmf_holding_reer", value=data['holding_mmf'][0]['holding_reer'])
+  state.setattr("sensor.finance_mmf_holding_reer" + ".unit_of_measurement", "$")
   
-  state.set("sensor.mmf_holding_cri", value=data['holding'][0]['holding_cri'])
-  state.setattr("sensor.mmf_holding_cri" + ".unit_of_measurement", "$")
+  state.set("sensor.finance_mmf_holding_cri", value=data['holding_mmf'][0]['holding_cri'])
+  state.setattr("sensor.finance_mmf_holding_cri" + ".unit_of_measurement", "$")
   
-  state.set("sensor.mmf_holding_total", value=data['holding'][0]['holding_total'])
-  state.setattr("sensor.mmf_holding_total" + ".unit_of_measurement", "$")
+  state.set("sensor.finance_mmf_holding_total", value=data['holding_mmf'][0]['holding_total'])
+  state.setattr("sensor.finance_mmf_holding_total" + ".unit_of_measurement", "$")
 
   for i in range(len(data['mmf'])):
-    state.set("sensor.mmf_" + data['mmf'][i]['ticker'], value=data['mmf'][i]['price'])
-    state.setattr("sensor.mmf_" + data['mmf'][i]['ticker']+ ".unit_of_measurement", "$")
-    state.setattr("sensor.mmf_" + data['mmf'][i]['ticker']+ ".holdingtotal", value=data['mmf'][i]['holding'])
+    state.set("sensor.finance_mmf_" + data['mmf'][i]['ticker'], value=data['mmf'][i]['price'])
+    state.setattr("sensor.finance_mmf_" + data['mmf'][i]['ticker']+ ".unit_of_measurement", "$")
+    state.setattr("sensor.finance_mmf_" + data['mmf'][i]['ticker']+ ".holdingtotal", value=data['mmf'][i]['holding'])
+  
+  for i in range(len(data['holding_stocks'])):
+    state.set("sensor.finance_stocks_" + data['holding_stocks'][i]['ticker'], value=data['holding_stocks'][i]['holding'])
+    state.setattr("sensor.finance_stocks_" + data['holding_stocks'][i]['ticker']+ ".unit_of_measurement", "shares")
+    state.setattr("sensor.finance_stocks_" + data['holding_stocks'][i]['ticker']+ ".price_paid", value=data['holding_stocks'][i]['price_paid'])
--- scripts/mutual.py
+++ scripts/mutual.py
@@ -41,39 +41,60 @@ mmf = [
     }
 ]
 
-data = {'mmf': [], 'holding': [], 'date': []}
-celi = 0
-reer = 0
-cri = 0
+stocks = [{
+    "ticker": "food_to",
+    "holding": 5,
+    "price_paid": 6.93
+},
+{
+    "ticker": "dm_v",
+    "holding": 75,
+    "price_paid": 0.19
+}
+]
+crypto = {
+  "btc": 0
+}
+
+data = {'mmf': [], 'holding_mmf': [], 'date': [],
+        'holding_stocks': [], 'holding_crypto': []}
+celi = 0.0
+reer = 0.0
+cri = 0.0
 
 current_time = datetime.datetime.now()
 
 for i in range(len(mmf)):
     r = requests.get(mmf[i]['url'],
-                     headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+      headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
     c = r.content
     soup = BeautifulSoup(c, "html.parser")
     # print(soup.prettify())
     price = soup.find("span", {"class": 'index-rank-value'}).text
 
-    holding = round(float(price) * mmf[i]['holding_celi']+ float(price) * mmf[i]['holding_reer'] + float(price) * mmf[i]['holding_cri'])
+    holding = round(float(price) * mmf[i]['holding_celi'] + float(
+        price) * mmf[i]['holding_reer'] + float(price) * mmf[i]['holding_cri'])
 
-    celi = celi + round(float(price) * mmf[i]['holding_celi'])
-    reer = reer + round(float(price) * mmf[i]['holding_reer'])
-    cri = cri + round(float(price) * mmf[i]['holding_cri'])
+    celi = celi + float(price) * mmf[i]['holding_celi']
+    reer = reer + float(price) * mmf[i]['holding_reer']
+    cri = cri + float(price) * mmf[i]['holding_cri']
     data['mmf'].append({'ticker': mmf[i]['ticker'],
                         'price': price,
                         'holding': holding})
 
+for i in range(len(stocks)):
+  data['holding_stocks'].append({"ticker": stocks[i]['ticker'],
+                                "holding": stocks[i]['holding'],
+                                'price_paid': stocks[i]['price_paid']})
+
 total = round(celi + reer + cri)
-data['holding'].append({"holding_celi": celi,
-             "holding_reer": reer,
-             "holding_cri": cri,
-             "holding_total": total
-             })
+data['holding_mmf'].append({"holding_celi": celi,
+                            "holding_reer": reer,
+                            "holding_cri": cri,
+                            "holding_total": total
+                            })
 data['date'].append({'date': str(current_time)})
-
-
+data['holding_crypto'].append(crypto)
 
 with open("/usr/share/hassio/homeassistant/data/mutual.json", "w") as writeJSON:
 #with open("mutual.json", "w") as writeJSON:
