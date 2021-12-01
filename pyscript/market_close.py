52ffe15354e6af6c67dfa731b5f582a752806838
petebdeblois <petebdeblois@gmail.com>
2021-12-01 02:51:02 -0500

    config files on 01-12-2021 02:51:01

--- pyscript/market_close.py
+++ pyscript/market_close.py
@@ -1,17 +1,17 @@
 import datetime
-now = datetime.datetime.now()
-print(now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-hour = now.hour
 
 #@time_trigger("once(17:52:30)")
-@time_trigger("cron(01 */1 * * *)")
+@time_trigger("cron(05 */1 * * *)")
 def market_state():
-    
-    if hour == 9:
-      state.set("binary_sensor.market_state", value='on')
-    elif hour == 16:
-      state.set("binary_sensor.market_state", value='off')
-    else:
-      pass
+  now = datetime.datetime.now()
+  print(now.year, now.month, now.day, now.hour, now.minute, now.second)
+  hour = now.hour
+  log.warning(hour)
+  if hour == 9:
+    state.set("binary_sensor.market_state", value='on')
+  elif hour == 16:
+    state.set("binary_sensor.market_state", value='off')
+  else:
+    pass
 
---
+++ scripts/test.py
@@ -0,0 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+print(now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+hour = now.hour
+print(hour)
